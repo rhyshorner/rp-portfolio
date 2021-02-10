868bd72c77e5e87565ae050646e3bf0d9f8e0e5b
Rhys Horner <rhys@ronallum.com>
2021-02-10 14:20:13 +1100

    absoulute filename pth added in projects admin for /img

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,18 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return 'Category: {}'.format(self.name)
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+
+    def __str__(self):
+        return 'Post: {}'.format(self.title)
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
